fb99989be49c35cc7a02d4cdf7229b70883059bc
Tolu <toluwanimi611@gmail.com>
2025-10-06 13:23:04 -0500

    Initilization of database on startup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Libarary_Cataloge_Program.Views;
+using Libarary_Cataloge_Program.Data;
 using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
 
 namespace Libarary_Cataloge_Program
 {
@@ -9,5 +11,39 @@ namespace Libarary_Cataloge_Program
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {}
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Initialize all databases
+            try
+            {
+                using (var context = new LibDataBase())
+                {
+                    context.Database.Migrate();
+                }
+                
+                using (var context = new AuthDb())
+                {
+                    context.Database.Migrate();
+                }
+                
+                using (var context = new Libs())
+                {
+                    context.Database.Migrate();
+                }
+                
+                using (var context = new ULDB())
+                {
+                    context.Database.Migrate();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Database initialization error: {ex.Message}\n\n{ex.StackTrace}", 
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
 }
---
+++ Migrations/AuthDbMigrations/20251006181347_u.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libarary_Cataloge_Program.Migrations.AuthDbMigrations
+{
+    /// <inheritdoc />
+    public partial class u : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
